14bdfe3017a0a983624ee43f4984bdfe8b2afc0b
Jay Dave <jaycdave@gmail.com>
2015-07-07 11:50:25 -0500

    added database and added a tableView populated with some dumby data

--- VacationDestination/ViewController.swift
+++ VacationDestination/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView! // linking the table view 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // correct you is - yoda
+
+        self.tableView.dataSource = self // needed for table view
+        self.tableView.delegate = self // needed for table view
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 // returning the number of rows
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell() // creating a varibale for each one of the cells
+        cell.textLabel?.text = "testing" // changing the text of each one of the rows to show some text
+        return cell // return the cell to render on the page
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // memory override
     }
-
-
 }
 
